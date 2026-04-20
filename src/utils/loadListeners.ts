d5b2202357f4f3039b96a43fa73dcd043f1ebc99
Ybson <ybsoncoelho22@gmail.com>
2026-04-19 21:56:41 -0300

    ✨ feat(build): add tsconfig.build.json and update package.json for build scripts 🔧 refactor(loadListeners): improve code formatting for better readability

--- src/utils/loadListeners.ts
+++ src/utils/loadListeners.ts
@@ -3,17 +3,17 @@ import { join } from "path";
 import type { App } from "@/structures/App";
 
 export const loadListeners = async (client: App): Promise<void> => {
-    const dir = join(import.meta.dirname, "../listeners");
+  const dir = join(import.meta.dirname, "../listeners");
 
-    for (const file of readdirSync(dir)) {
-        if (!file.endsWith(".ts")) continue;
+  for (const file of readdirSync(dir)) {
+    if (!file.endsWith(".ts")) continue;
 
-        const filePath = join(dir, file);
-        const mod = await import(filePath);
+    const filePath = join(dir, file);
+    const mod = await import(filePath);
 
-        if (mod.default) {
-            const event = file.replace(".ts", "");
-            client.on(event, (...args) => mod.default(client, ...args));
-        }
+    if (mod.default) {
+      const event = file.replace(".ts", "");
+      client.on(event, (...args) => mod.default(client, ...args));
     }
+  }
 };
